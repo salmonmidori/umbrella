9f420bfe958fe6fe826592569b81d407489bc336
Amy May Nguyen <anguye10@uchicago.edu>
2025-01-26 21:20:50 +0000

    Completed main instructions

--- umbrella.rb
+++ umbrella.rb
@@ -9,49 +9,43 @@ puts "========================================
 
 Where are you?
 "
-# user_location = gets.chomp
-# user_location = "Chicago"
-# gmaps_key = ENV["GMAPS_KEY"]
-# gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps_key}"
-# lon = 41.8887
-# lat = -87.6297982
+user_location = gets.chomp
+gmaps_key = ENV["GMAPS_KEY"]
+gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{gmaps_key}"
+gmaps_resp = HTTP.get(gmaps_url).to_s
+gmaps_parsed_resp = JSON.parse(gmaps_resp)
+def coordinates(parsed_resp, type)
+  parsed_resp.dig("results", 0, "geometry", "location", type)
+end
+lng = coordinates(gmaps_parsed_resp, "lng")
+lat = coordinates(gmaps_parsed_resp, "lat")
 
-# pirate_weather_key = ENV["PIRATE_WEATHER_KEY"]
-# pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_key}/#{lon},#{lat}"
-# pirate_weather_resp = HTTP.get(pirate_weather_url).to_s
-# pirate_weather_parsed_resp = JSON.parse(pirate_weather_resp)
-# temp = pirate_weather_parsed_resp["currently"]["temperature"]
-# weather = pirate_weather_parsed_resp["currently"]["summary"]
-pp Time.at(1737851280)
-BREAK
+pirate_weather_key = ENV["PIRATE_WEATHER_KEY"]
+pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_key}/#{lat},#{lng}"
+pirate_weather_resp = HTTP.get(pirate_weather_url).to_s
+pirate_weather_parsed_resp = JSON.parse(pirate_weather_resp)
 
-rain0 = 53
-rain1 = 37
-rain2 = 18
+temp = pirate_weather_parsed_resp["currently"]["temperature"]
+weather = pirate_weather_parsed_resp.dig("hourly", "data", 0, "summary")
 
-if weather == "Clear"
+def rain_prob(parsed_resp, hour_num)
+  parsed_resp.dig("hourly", "data", hour_num, "precipProbability")
+end
+
+
+if rain_prob(pirate_weather_parsed_resp, 0) < 0.1 && rain_prob(pirate_weather_parsed_resp, 1) < 0.05 && rain_prob(pirate_weather_parsed_resp, 2) < 0.05
   rain_text = "You probably won't need an umbrella."
 else
-  rain_text = "In 0 hours, there is a #{rain0}% chance of precipitation.\nIn 1 hours, there is a #{rain1}% chance of precipitation.\nIn 2 hours, there is a #{rain2}% chance of precipitation.\nYou might want to take an umbrella!"
+  rain_text = "You might want to take an umbrella!"
 end
 
 puts "Checking the weather at #{user_location}....
-Your coordinates are #{longitude}, #{latitude}.
-It is currently #{temp}°F.
-Next hour: #{weather}
+Your coordinates are #{lat}, #{lng}.
+It is currently #{temp}°F and the weather is #{weather.downcase}. There is a #{rain_prob(pirate_weather_parsed_resp, 0) * 100}% chance of precipitation.
+In 1 hour, there is a #{rain_prob(pirate_weather_parsed_resp, 1) * 100}% chance of precipitation.
+In 2 hours, there is a #{rain_prob(pirate_weather_parsed_resp, 2) * 100}% chance of precipitation.
 " + rain_text
 
-# Ask the user for their location. (Recall gets.)
-   
-#   Get and store the user’s location.
-    
-#   Get the user’s latitude and longitude from the Google Maps API.
-    
-#   Get the weather at the user’s coordinates from the Pirate Weather API.
-    
-#   Display the current temperature and summary of the weather for the next hour.
-    
-  
 
 
 # If you get that far, then stretch further:
